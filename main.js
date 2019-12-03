3fc6db3064a0272d91bd4fd5bf5f70498ced064f
Viktoria Radchenko <viktoriia.rad@gmail.com>
2019-12-03 16:44:56 +0100

    edit js kod

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const leftElemnt = document.querySelector('.leftBenefits');
-const centerElemnt = document.querySelector('.centerBenefits');
-const rightElemnt = document.querySelector('.rightBenefits');
-const benefits = document.querySelector('.benefits');
-const headerContact = document.querySelector('.header__contact');
+// const leftElemnt = document.querySelector('.leftBenefits');
+// const centerElemnt = document.querySelector('.centerBenefits');
+// const rightElemnt = document.querySelector('.rightBenefits');
+// const benefits = document.querySelector('.benefits');
+// const headerContact = document.querySelector('.header__contact');
 const nav = document.querySelector('.navigation');
 const navLogo = document.querySelector('.navigation__logo-img');
 const burgerNavLogo = document.querySelector('.burger-menu__logo-img');
@@ -19,6 +19,7 @@ const burgerWorksLink = document.querySelector('.burger-menu__workslink');
 const burgerOfferLink = document.querySelector('.burger-menu__offerlink');
 const burgerCollaborationLink = document.querySelector('.burger-menu__collaborationlink');
 
+const headerSection = document.querySelector('.header');
 const aboutMeSection = document.querySelector('.about-me');
 const ourWorksSection = document.querySelector('.our-works');
 const offerSection = document.querySelector('.offer');
@@ -26,13 +27,13 @@ const collaborationSection = document.querySelector('.collaboration');
 const navLogoHeight = navLogo.offsetHeight * 0.6;
 
 
-const showBenefits = () => {
-  if ((window.outerHeight + window.pageYOffset) >= benefits.offsetHeight + benefits.offsetTop) {
-    leftElemnt.classList.add('benefits__container--active');
-    centerElemnt.classList.add('benefits__container--active');
-    rightElemnt.classList.add('benefits__container--active');
-  }
-}
+// const showBenefits = () => {
+//   if ((window.outerHeight + window.pageYOffset) >= benefits.offsetHeight + benefits.offsetTop) {
+//     leftElemnt.classList.add('benefits__container--active');
+//     centerElemnt.classList.add('benefits__container--active');
+//     rightElemnt.classList.add('benefits__container--active');
+//   }
+// }
 
 
 const scrollToHandler = (element) => {
@@ -48,27 +49,40 @@ const scrollToHandler = (element) => {
 };
 
 const onScrollNavMenu = () => {
-  const aboutStart = benefits.offsetTop + benefits.offsetHeight - navLogoHeight;
-  const aboutEnd = aboutStart + aboutMeSection.offsetHeight;
-  const worksStart = aboutEnd;
-  const worksEnd = worksStart + ourWorksSection.offsetHeight;
-  const offerStart = worksEnd;
+
+  const offerStart = headerSection.offsetTop + headerSection.offsetHeight - navLogoHeight;
   const offerEnd = offerStart + offerSection.offsetHeight;
-  const collaborationStart = offerEnd;
+
+  const worksStart = offerEnd;
+  const worksEnd = worksStart + ourWorksSection.offsetHeight;
+
+  const aboutStart = worksEnd;
+  const aboutEnd = aboutStart + aboutMeSection.offsetHeight;
+
+  const collaborationStart = aboutEnd;
   const collaborationEnd = collaborationStart + collaborationSection.offsetHeight;
 
+  // const aboutStart = headerSection.offsetTop + headerSection.offsetHeight - navLogoHeight;
+  // const aboutEnd = aboutStart + aboutMeSection.offsetHeight;
+  // const worksStart = aboutEnd;
+  // const worksEnd = worksStart + ourWorksSection.offsetHeight;
+  // const offerStart = worksEnd;
+  // const offerEnd = offerStart + offerSection.offsetHeight;
+  // const collaborationStart = offerEnd;
+  // const collaborationEnd = collaborationStart + collaborationSection.offsetHeight;
+
   mainLink.classList.remove('navigation__link--hover');
-  aboutLink.classList.remove('navigation__link--hover');
-  worksLink.classList.remove('navigation__link--hover');
   offerLink.classList.remove('navigation__link--hover');
+  worksLink.classList.remove('navigation__link--hover');
+  aboutLink.classList.remove('navigation__link--hover');
   collaborationLink.classList.remove('navigation__link--hover');
 
-  if (window.pageYOffset >= aboutStart && window.pageYOffset < aboutEnd) {
-    aboutLink.classList.add('navigation__link--hover');
+  if (window.pageYOffset >= offerStart && window.pageYOffset < offerEnd) {
+    offerLink.classList.add('navigation__link--hover');
   } else if (window.pageYOffset >= worksStart && window.pageYOffset < worksEnd) {
     worksLink.classList.add('navigation__link--hover');
-  } else if (window.pageYOffset >= offerStart && window.pageYOffset < offerEnd) {
-    offerLink.classList.add('navigation__link--hover');
+  } else if (window.pageYOffset >= aboutStart && window.pageYOffset < aboutEnd) {
+    aboutLink.classList.add('navigation__link--hover');
   } else if (window.pageYOffset >= collaborationStart && window.pageYOffset < collaborationEnd) {
     collaborationLink.classList.add('navigation__link--hover');
   } else {
@@ -79,7 +93,7 @@ const onScrollNavMenu = () => {
 
 document.addEventListener('scroll', function () {
 
-  if (window.pageYOffset > headerContact.offsetHeight) {
+  if (window.pageYOffset > 0) {
     nav.classList.add('navigation--active');
     navLogo.classList.add('navigation__logo-img--active');
   } else {
@@ -87,25 +101,26 @@ document.addEventListener('scroll', function () {
     navLogo.classList.remove('navigation__logo-img--active');
   }
 
-  showBenefits();
+  // showBenefits();
+
   onScrollNavMenu();
 
 });
 
-aboutLink.addEventListener('click', () => scrollToHandler(benefits));
-worksLink.addEventListener('click', () => scrollToHandler(aboutMeSection));
-offerLink.addEventListener('click', () => scrollToHandler(ourWorksSection));
-collaborationLink.addEventListener('click', () => scrollToHandler(offerSection));
+offerLink.addEventListener('click', () => scrollToHandler(headerSection));
+worksLink.addEventListener('click', () => scrollToHandler(offerSection));
+aboutLink.addEventListener('click', () => scrollToHandler(ourWorksSection));
+collaborationLink.addEventListener('click', () => scrollToHandler(aboutMeSection));
 mainLink.addEventListener('click', () => window.scrollTo({
   top: 0,
   left: 0,
   behavior: 'smooth'
 }));
 
-burgerAboutLink.addEventListener('click', () => scrollToHandler(benefits));
-burgerWorksLink.addEventListener('click', () => scrollToHandler(aboutMeSection));
-burgerOfferLink.addEventListener('click', () => scrollToHandler(ourWorksSection));
-burgerCollaborationLink.addEventListener('click', () => scrollToHandler(offerSection));
+burgerOfferLink.addEventListener('click', () => scrollToHandler(headerSection));
+burgerWorksLink.addEventListener('click', () => scrollToHandler(offerSection));
+burgerAboutLink.addEventListener('click', () => scrollToHandler(ourWorksSection));
+burgerCollaborationLink.addEventListener('click', () => scrollToHandler(aboutMeSection));
 burgerMainLink.addEventListener('click', () => window.scrollTo({
   top: 0,
   left: 0,
