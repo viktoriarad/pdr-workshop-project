5715f71c4256a1f60f7f33d97997d83de9ad27d7
Viktoria Radchenko <viktoriia.rad@gmail.com>
2019-12-08 16:35:28 +0100

    add section about pdr, edit section galary and collaboration

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ const navLogo = document.querySelector('.navigation__logo-img');
 const burgerNavLogo = document.querySelector('.burger-menu__logo-img');
 
 const mainLink = document.querySelector('.mainlink');
+const aboutPdrLink = document.querySelector('.aboutpdrlink')
 const aboutLink = document.querySelector('.aboutlink');
 const worksLink = document.querySelector('.workslink');
 const offerLink = document.querySelector('.offerlink');
 const collaborationLink = document.querySelector('.collaborationlink');
 
 const burgerMainLink = document.querySelector('.burger-menu__mainlink');
+const burgerAboutPdrLink = document.querySelector('.burger-menu__aboutpdrlink');
 const burgerAboutLink = document.querySelector('.burger-menu__aboutlink');
 const burgerWorksLink = document.querySelector('.burger-menu__workslink');
 const burgerOfferLink = document.querySelector('.burger-menu__offerlink');
 const burgerCollaborationLink = document.querySelector('.burger-menu__collaborationlink');
 
 const headerSection = document.querySelector('.header');
+const aboutPdrSection = document.querySelector('.aboutpdr');
 const aboutMeSection = document.querySelector('.about-me');
 const ourWorksSection = document.querySelector('.our-works');
 const offerSection = document.querySelector('.offer');
@@ -43,8 +46,11 @@ const bookmarksOnLoadingHandler = () => {
   }
 
   switch (bookmark) {
-    case 'offer':
+    case 'aboutpdr':
       scrollToHandler(headerSection);
+      break;
+    case 'offer':
+      scrollToHandler(aboutPdrSection);
       break;
     case 'works':
       scrollToHandler(offerSection);
@@ -60,7 +66,10 @@ const bookmarksOnLoadingHandler = () => {
 
 const onScrollNavMenu = () => {
 
-  const offerStart = headerSection.offsetTop + headerSection.offsetHeight - navLogoHeight;
+  const aboutPdrStart = Math.floor(headerSection.offsetTop + headerSection.offsetHeight - navLogoHeight);
+  const aboutPdrEnd = aboutPdrStart + aboutPdrSection.offsetHeight;
+
+  const offerStart = aboutPdrEnd;
   const offerEnd = offerStart + offerSection.offsetHeight;
 
   const worksStart = offerEnd;
@@ -73,12 +82,15 @@ const onScrollNavMenu = () => {
   const collaborationEnd = collaborationStart + collaborationSection.offsetHeight;
 
   mainLink.classList.remove('navigation__link--hover');
+  aboutPdrLink.classList.remove('navigation__link--hover');
   offerLink.classList.remove('navigation__link--hover');
   worksLink.classList.remove('navigation__link--hover');
   aboutLink.classList.remove('navigation__link--hover');
   collaborationLink.classList.remove('navigation__link--hover');
 
-  if (window.pageYOffset >= offerStart && window.pageYOffset < offerEnd) {
+  if (window.pageYOffset >= aboutPdrStart && window.pageYOffset < aboutPdrEnd) {
+    aboutPdrLink.classList.add('navigation__link--hover');
+  } else if (window.pageYOffset >= offerStart && window.pageYOffset < offerEnd) {
     offerLink.classList.add('navigation__link--hover');
   } else if (window.pageYOffset >= worksStart && window.pageYOffset < worksEnd) {
     worksLink.classList.add('navigation__link--hover');
@@ -106,7 +118,8 @@ document.addEventListener('scroll', function () {
 
 });
 
-offerLink.addEventListener('click', () => scrollToHandler(headerSection));
+aboutPdrLink.addEventListener('click', () => scrollToHandler(headerSection));
+offerLink.addEventListener('click', () => scrollToHandler(aboutPdrSection));
 worksLink.addEventListener('click', () => scrollToHandler(offerSection));
 aboutLink.addEventListener('click', () => scrollToHandler(ourWorksSection));
 collaborationLink.addEventListener('click', () => scrollToHandler(aboutMeSection));
@@ -116,7 +129,8 @@ mainLink.addEventListener('click', () => window.scrollTo({
   behavior: 'smooth'
 }));
 
-burgerOfferLink.addEventListener('click', () => scrollToHandler(headerSection));
+burgerAboutPdrLink.addEventListener('click', () => scrollToHandler(headerSection));
+burgerOfferLink.addEventListener('click', () => scrollToHandler(aboutPdrSection));
 burgerWorksLink.addEventListener('click', () => scrollToHandler(offerSection));
 burgerAboutLink.addEventListener('click', () => scrollToHandler(ourWorksSection));
 burgerCollaborationLink.addEventListener('click', () => scrollToHandler(aboutMeSection));
